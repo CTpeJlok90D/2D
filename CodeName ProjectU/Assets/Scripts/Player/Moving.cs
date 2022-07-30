866200fde956a204c82c69add52b627536ff4fb0
1DhT <max.egorov01@yandex.ru>
2022-07-30 19:34:53 +0300

    this is not working
    SAD :disappointed:

--- CodeName ProjectU/Assets/Scripts/Player/Moving.cs
+++ CodeName ProjectU/Assets/Scripts/Player/Moving.cs
@@ -7,12 +7,20 @@ public class Moving : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _forceJump;
     [SerializeField] private float _boost;
+    [SerializeField] private float _jumpCooldown;
     [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private AnimationCurve _jumpTraectory;
 
     private float _correctSpeed;
+    private float _cantJumpSec;
+    private float _correctJumpState;
+    private float _hight;
+    private bool _onGround;
     private WayDirectrion _way;
     private SpriteRotator _spriteRotator;
     private Direction _direction = Direction.Right;
+
 
     private void Awake()
     {
@@ -23,17 +31,39 @@ public class Moving : MonoBehaviour
     private void FixedUpdate()
     {
         Walk();
+        Jump();
+        if (_cantJumpSec > 0)
+        {
+            _cantJumpSec -= Time.fixedDeltaTime;
+        }
+    }
+    private void Update()
+    {
+        if (_correctJumpState > _jumpTraectory.keys[_jumpTraectory.keys.Length - 1].time)
+        {
+            _correctJumpState = 0;
+        }
+        if (_correctJumpState > 0)
+        {
+            _correctJumpState += Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, _hight + _jumpTraectory.Evaluate(_correctJumpState));
+        }
     }
     private void Walk()
     {
         float move = Input.GetAxis("Horizontal");
-        if (move == 0) 
+        if (move == 0)
         {
-            _correctSpeed = 0;  
-            return; 
+            _correctSpeed = 0;
+            return;
         }
         _direction = move > 0 ? Direction.Right : Direction.Left;
         _spriteRotator.RotateBody(_direction);
+        if (_onGround == false)
+        {
+            transform.position += new Vector3(move, 0) * _correctSpeed * Time.fixedDeltaTime;
+            return;
+        }
         Vector2 direction = _way.GetDirection(_direction);
         if (Input.GetAxis("Sprint") > 0 && _correctSpeed <= _maxSpeed)
         {
@@ -43,10 +73,29 @@ public class Moving : MonoBehaviour
                 _correctSpeed = _maxSpeed;
             }
         }
-        else 
-        { 
-            _correctSpeed = _speed; 
+        else
+        {
+            _correctSpeed = _speed;
         }
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.fixedDeltaTime * _correctSpeed;
+        Debug.Log(move);
+        _rigidbody2D.velocity = new Vector2(move, 0) * Time.fixedDeltaTime;
+    }
+    private void Jump()
+    {
+        if (Input.GetAxis("Vertical") > 0 && _onGround && _cantJumpSec <= 0)
+        {
+            _correctJumpState += 0.01f;
+            _hight = transform.position.y;
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _onGround = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), Vector3.down, 0.1f).collider != null;
+        _correctJumpState = 0;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _onGround = false;
     }
 }

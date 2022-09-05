084be7e1bb0252db97eb1172ed1b9e9366700aa0
CTpeJlok90D <S90Dima@yandex.com>
2022-09-05 19:32:04 +0300

    _

---
+++ ProjectU/Assets/Scripts/Health/Influence.cs
@@ -0,0 +1,8 @@
+﻿public class Influence
+{
+    public float GivingBoodLevelPerSecond = 0f;
+    public float FilteringBloodPerSecond = 0f;
+    public float BloodPumping = 0f;
+    public float GivingOxygen = 0f;
+    public float GivingOxygenStorage = 0f;
+}
---
+++ ProjectU/Assets/Scripts/Health/Lung.cs
@@ -0,0 +1,10 @@
+﻿public class Lung : BodyPart
+{
+    private float _givingOxygen = 6f;
+    private float _givingOxygenStorage = 50f;
+
+    public override Influence GetInfluence()
+    {
+        return new() { GivingOxygen = _givingOxygen * HeatPoint, GivingOxygenStorage = _givingOxygenStorage * HeatPoint };
+    }
+}
